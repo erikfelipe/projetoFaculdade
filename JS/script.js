2af25f4c96f32769dea44fc344a9ca27b5e70c03
Erik Felipe <63510979+erikfelipe@users.noreply.github.com>
2020-11-26 20:47:40 -0300

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -13,3 +13,15 @@ olho.addEventListener('mouseup', function(){
 olho.addEventListener('mousemove',function(){
     senha.type = 'password';
 })
+
+function ValidarCadastro(){
+    email_cad = document.getElementById("inputEmail_cad").value;
+    senha_cad = document.getElementById("inputSenha_cad").value;
+    nome_cad = document.getElementById("inputNome").value;
+    numero_cad = document.getElementById("inputNumero").value;
+    estado_cad = document.getElementById("inputEstado").value;
+
+    if(email_cad == "" || senha_cad == "" || nome_cad == "" || numero_cad == ""){
+        alert("Campos não podem ficar vazios")
+    }
+}
