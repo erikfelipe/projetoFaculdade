33e784e0a6e664de7355eac3024f56390f05b18d
Erik Felipe <63510979+erikfelipe@users.noreply.github.com>
2020-12-01 21:24:20 -0300

    Add files via upload

--- atualizar.php
+++ atualizar.php
@@ -8,16 +8,16 @@ $sql = "select * from tb_user where email = '" . $email_logado . "';";
 $query = $conn->prepare($sql);
 $query->execute(array($email));
 $data = $query->fetch(PDO::FETCH_ASSOC);
-$nome = $data['nome'];
-$numero = $data['numero'];
-$email = $data['email'];
+$nome = $data['inputNome'];
+$numero = $data['inputTel'];
+$email = $data['inputEmail'];
 ?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>  <meta charset="UTF-8">    <title>Document</title> </head>
 <body>
-<form action="atualizar.php?email=<?php echo $email ?>" method="POST">
+<form action="atualizar.php?email=<?php echo $email ?>" method="GET">
         email: <input type ="text" name = "inputEmail" value=<?php echo $email; ?>><br>
         Digite o nome: <input type="text" name = "inputNome" value="<?php echo $nome; ?>"><br>
         Selecione o telefone: <input type="text" name="inputTel" value="<?php echo $numero; ?>"><br>
